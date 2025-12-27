5c4d10b2bdcebc37794a94b32751d4c4117ce29c
Alexara1 <146883327+Alexara1@users.noreply.github.com>
2025-12-27 23:05:44 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,3 +15,17 @@ export default defineConfig({
     historyApiFallback: true,
   }
 });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    historyApiFallback: true,
+  }
+});
